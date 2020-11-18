71384f11175e9f283f1c7ad5cd7cf2231cf23db6
Smeki <smekika@yahoo.com>
2020-11-18 10:58:32 +0000

    My initial code check in for devOpsautomation

---
+++ Hello.java
@@ -0,0 +1,6 @@
+// This App will send updates on our DevOpsautomation project
+Public cass HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("Hello, World!"); 
+  }  
+}
